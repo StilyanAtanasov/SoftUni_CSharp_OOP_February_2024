7afaba2bcd3b9af65df9436c1ee940abbcef0dbf
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-25 11:27:22 +0200

    Added Solution

---
+++ 03. Encapsulation/Solutions/01. Persons/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int lines = int.Parse(Console.ReadLine()!);
+        List<Person> persons = new();
+        for (int i = 0; i < lines; i++)
+        {
+            string[] personInfo = Console.ReadLine()!.Split();
+            persons.Add(new(personInfo[0], personInfo[1], int.Parse(personInfo[2])));
+        }
+
+        persons.OrderBy(p => p.FirstName).ThenBy(p => p.Age).ToList().ForEach(Console.WriteLine);
+    }
+}
