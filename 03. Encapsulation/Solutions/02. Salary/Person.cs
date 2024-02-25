829a833c0743a6f35940437d3f40a243a349c35a
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-25 11:45:20 +0200

    Added Solution

---
+++ 03. Encapsulation/Solutions/02. Salary/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonsInfo;
+
+public class Person
+{
+    public Person(string firstName, string lastName, int age, decimal salary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Salary = salary;
+    }
+
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int Age { get; }
+    public decimal Salary { get; private set; }
+
+    public void IncreaseSalary(decimal percentage)
+    {
+        if (Age < 30) Salary *= 1 + percentage / 200; // get half the increase
+        else Salary *= 1 + percentage / 100;
+    } 
+
+    public override string ToString() => $"{FirstName} {LastName} receives {Salary:F2} leva.";
+}
---
+++ 03. Encapsulation/Solutions/02. Salary/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace PersonsInfo;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        int lines = int.Parse(Console.ReadLine()!);
+        List<Person> persons = new();
+        for (int i = 0; i < lines; i++)
+        {
+            string[] personInfo = Console.ReadLine()!.Split();
+            persons.Add(new(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3])));
+        }
+
+        decimal percentage = decimal.Parse(Console.ReadLine()!);
+        persons.ForEach(p => p.IncreaseSalary(percentage));
+        persons.ForEach(Console.WriteLine);
+    }
+}
