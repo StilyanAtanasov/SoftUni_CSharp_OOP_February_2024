6e680c1c381b92d44b603507a49dad0863940b82
Stilyan Atanasov <stilyan2008@gmail.com>
2024-03-02 18:51:25 +0200

    Added Solution

---
+++ 05. Interfaces and Abstraction/Solutions/02. Cars/Seat.cs
@@ -0,0 +1,18 @@
+﻿namespace Cars;
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        Model = model;
+        Color = color;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+
+    public string Start() => "Engine start";
+    public string Stop() => "Breaaak!";
+
+    public override string ToString() => $"{Color} Seat {Model}";
+}
---
+++ 05. Interfaces and Abstraction/Solutions/02. Cars/Tesla.cs
@@ -0,0 +1,20 @@
+﻿namespace Cars;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        Model = model;
+        Color = color;
+        Battery = battery;
+    }
+
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Battery { get; set; }
+
+    public string Start() => "Engine start";
+    public string Stop() => "Breaaak!";
+
+    public override string ToString() => $"{Color} Tesla {Model} with {Battery} Batteries";
+}
